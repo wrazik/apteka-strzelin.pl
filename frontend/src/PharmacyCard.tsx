fd0da8e8f2cf8fd199b78cc5be611fb1e4831fdb
wrazik <wojciech.razik@gmail.com>
2022-02-04 17:06:57 +0100

    Added pharmacy images, view changed to card

--- frontend/src/PharmacyCard.tsx
+++ frontend/src/PharmacyCard.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import images from "./img/index";
 
 interface Pharmacy {
-    name: String,
-    address: String,
-    phone: String,
+    name: string,
+    address: string,
+    phone: string,
 }
+export type {Pharmacy};
 
-export type { Pharmacy };
+const name_to_img: Record<string, string> = {
+    "Apteka Pod Paprocią": images.pod_paprocia,
+    "Inter Apteka": images.inter_apteka,
+    "Apteka Nova": images.nova,
+    "Apteka Kalina": images.kalina,
+    "Apteka Pod Złotym Lwem": images.pod_zlotym_lwem,
+    "Apteka Centrum Zdrowia": images.centrum_zdrowia
+}
 
 export function PharmacyCard(props: Pharmacy) {
     return (
-        <div className="current-pharmacy">
-            <div className="welcome-msg"> Dyżur pełni: </div>
-            <div className="pharmacy-name"> {props.name} </div>
-            <div className="pharmacy-address"> {props.address} </div>
-            <div className="pharmacy-phone"> {props.phone} </div>
-        </div>
+        <Card sx={{ maxWidth: 400 , boxShadow: 20, margin: "auto auto"}}>
+            <CardMedia
+                component="img"
+                image={name_to_img[props.name]}
+            />
+            <CardContent>
+                <Typography variant="body2" color="red">
+                    Dyżur
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.address}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.phone}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
---
+++ frontend/src/img/index.tsx
@@ -0,0 +1,15 @@
+import pod_paprocia from "./pod_paprocia.jpg"
+import inter_apteka from "./inter_apteka.jpg"
+import centrum_zdrowia from "./centrum_zdrowia.jpg"
+import kalina from "./kalina.jpg"
+import nova from "./nova.jpg"
+import pod_zlotym_lwem from "./pod_zlotym_lwem.jpg"
+
+export default {
+    pod_paprocia,
+    inter_apteka,
+    centrum_zdrowia,
+    kalina,
+    nova,
+    pod_zlotym_lwem
+}
