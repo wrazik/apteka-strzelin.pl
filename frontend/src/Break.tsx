208222705715943fbd8722a518a4b4d308aceedf
wrazik <wojciech.razik@gmail.com>
2022-12-30 13:20:43 +0100

    Changed break description

--- frontend/src/Break.tsx
+++ frontend/src/Break.tsx
@@ -26,14 +26,26 @@ export function Break() {
                 />
                 <CardContent>
                     <Typography variant="body2" color="red">
-                        <h1> Przerwa techniczna!!!!! </h1>
+                        <h1> Zmiany, zmiany.... </h1>
                     </Typography>
                     <Typography gutterBottom variant="h5" component="div">
-                        Prace w trakcie
+                        Przerwa techniczna!
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         Niestety, Gmina Strzelin zmieniła sposób dyżurowania aptek. Obecnie, rozpisany jest on na konkretne dni. Ta strona bazowała na podziale tygodniowym, stąd potrzebuję czasu na dostosowanie.
                         Proszę o cierpliwość!
+
+                        Najbliższe dyżury:
+                        <br/>
+                        30.12 - Pod Złotym Lwem
+                        <br/>
+                        31.12 - Pod Złotym Lwem
+                        <br/>
+                        01.01 - Apteka Nova
+                        <br/>
+                        02.01 - Kalina
+                        <br/>
+                        03.01 - Pod Paprocią
                     </Typography>
                 </CardContent>
             </Card>
