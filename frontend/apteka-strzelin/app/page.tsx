346952eeb87ddc893a5dad8b987cf8a1e2efc197
wrazik <wojciech.razik@gmail.com>
2024-04-07 21:16:08 +0200

    new version, new me

--- frontend/apteka-strzelin/app/page.tsx
+++ frontend/apteka-strzelin/app/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import * as React from "react";
+import {NextUIProvider} from "@nextui-org/react";
+
 export default function Home() {
   return (
-      <h1>Hello, Next.js!</h1>
-  );
+      <NextUIProvider>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+            <h1> Home page</h1>
+        </div>
+      </NextUIProvider>
+    )
 }
-
